e7e21e3da89375e73fdf44848f245c73400b144a
unknown <liangjiwei-os@service.360ityun.com>
2019-04-28 19:14:58 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  created () {
+    console.log('小程序启动了')
+  }
+}
+</script>
+
+<style>
+.text-footer{
+  text-align: center;
+  font-size: 12px;
+  margin-bottom:5px;
+}
+.text-primary{
+  color:#EA5149;
+}
+.right {
+  float: right;
+}
+.page-title {
+  padding-left: 20px;
+  background-color: #eee;
+  line-height: 40px;
+  font-size: 14px;
+}
+.btn{
+  color:white;
+  background:#EA5A49;
+  margin-bottom: 10px;
+  padding-left: 15px;
+  padding-left: 15px;
+  border-radius: 2px;
+  font-size: 16px;
+  line-height: 40px;
+  height: 40px;
+  width: 100%;
+}
+.btn:active{
+  background: #FA5A49;
+}
+</style>
---
+++ src/pages/me/Me.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="container">
+    <div class="userinfo">
+      <img :src="userinfo.avatarUrl" alt="">
+      <button open-type="getUserInfo" lang="zh_CN" @getuserinfo="login">{{userinfo.nickName}}</button>
+    </div>
+    <YearProgress></YearProgress>
+
+    <button v-if="userinfo.openId" @click='scanBook' class='btn'>添加图书</button>
+  </div>
+</template>
+<script>
+import qcloud from 'wafer2-client-sdk'
+import YearProgress from '@/components/YearProgress'
+import {showSuccess, post, showModal} from '@/util'
+import config from '@/config'
+// import { constants } from 'http2';
+export default {
+  components: {
+    YearProgress
+  },
+  data () {
+    return {
+      userinfo: {
+        avatarUrl: '../../../static/img/unlogin.png',
+        nickName: '点击登录'
+      }
+    }
+  },
+  methods: {
+    async addBook (isbn) {
+      console.log(isbn)
+      const res = await post('/weapp/addbook', {
+        isbn,
+        openid: this.userinfo.openId
+      })
+      showModal('添加成功', `${res.title}添加成功`)
+    },
+    scanBook () {
+      wx.scanCode({
+        success: (res) => {
+          if (res.result) {
+            this.addBook(res.result)
+          }
+        }
+      })
+    },
+    loginSuccess (res) {
+      showSuccess('登录成功')
+      wx.setStorageSync('userinfo', res)
+      this.userinfo = res
+    },
+    login () {
+      wx.showToast({
+        title: '登录中',
+        icon: 'loading'
+      })
+      qcloud.setLoginUrl(config.loginUrl)
+      const session = qcloud.Session.get()
+      if (session) {
+        qcloud.loginWithCode({
+          success: res => {
+            console.log('没过期的登录', res)
+            this.loginSuccess(res)
+          },
+          fail: err => {
+            console.error(err)
+          }
+        })
+      } else {
+        qcloud.login({
+          success: res => {
+            console.log('登录成功', res)
+            this.loginSuccess(res)
+          },
+          fail: err => {
+            console.error(err)
+          }
+        })
+      }
+    }
+  },
+  onShow () {
+    wx.showShareMenu()
+    let userinfo = wx.getStorageSync('userinfo')
+    // console.log([userinfo])
+    if (userinfo) {
+      this.userinfo = userinfo
+    }
+    // console.log(this.userinfo)
+  }
+}
+</script>
+
+<style lang='scss'>
+.container{
+  padding:0 30rpx;
+
+}
+.userinfo{
+  margin-top:100rpx;
+  text-align:center;
+  img{
+    width: 150rpx;
+    height:150rpx;
+    margin: 20rpx;
+    border-radius: 50%;
+  }
+}
+
+
+</style>
